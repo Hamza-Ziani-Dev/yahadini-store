facd42a1165c77732e968c8ea2c61d37b0849922
Hamza-Ziani-Dev <hamzaziani.dev@gmail.com>
2024-07-16 13:35:59 +0100

    "add interface and call data all products"

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ProductsModule } from '../../products.module';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-all-products',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductsModule,
+  ],
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.css'
 })
 export class AllProductsComponent {
+  products : any[] = [];
+
+
+  constructor(private productsService : ProductsService) {}
+
+
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+
+  // get All Products 
+  getAllProducts(){
+    this.productsService.getAllProducts().subscribe((res:any)=>{
+      this.products = res
+      console.log(this.products);
+    });
+    
+  }
 
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private HttpClient : HttpClient) { }
+
+
+  getAllProducts(){
+    return this.HttpClient.get('https://fakestoreapi.com/products');}
 }
